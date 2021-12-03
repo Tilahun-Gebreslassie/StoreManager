6761a797a27d1563488f6be9667c496d34b1819a
tilahungl89 <tilahungl89@gmail.com>
2021-12-03 07:53:15 +0100

    [StoreManager] Add Account controller MVC
    - User login, register user
    - User can login and view available items from store

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using ProjectMVC5.Models.StoreManager.Models;
+using StoreManager.Data;
+
+namespace StoreManager.Models
+{
+    public class AccountController : Controller
+    {
+        private StoreManagerContext db = new StoreManagerContext();
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View(db.LogIns.ToList());
+        }
+
+        // GET: Account/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LogIn logIn = db.LogIns.Find(id);
+            if (logIn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logIn);
+        }
+
+        // GET: Account/Create
+        [ActionName ("Register")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Register")]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Email,Password, ConfirmPassword")] LogIn logIn)
+        {
+            if (ModelState.IsValid)
+            {
+                logIn.Password = GetMD5(logIn.Password);
+                db.Configuration.ValidateOnSaveEnabled = false;
+                db.LogIns.Add(logIn);
+                db.SaveChanges();
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(logIn);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string email, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_password = GetMD5(password);
+                var data = db.LogIns.Where(s => s.Email.Equals(email) && s.Password.Equals(f_password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FirstName + " " + data.FirstOrDefault().LastName;
+                    Session["Email"] = data.FirstOrDefault().Email;
+                    Session["Id"] = data.FirstOrDefault().Id;
+                    return RedirectToAction("Index", "Store");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LogIn logIn = db.LogIns.Find(id);
+            if (logIn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logIn);
+        }
+
+        // POST: Account/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idUser,FirstName,LastName,Email,Password")] LogIn logIn)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(logIn).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(logIn);
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            LogIn logIn = db.LogIns.Find(id);
+            if (logIn == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logIn);
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            LogIn logIn = db.LogIns.Find(id);
+            db.LogIns.Remove(logIn);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
